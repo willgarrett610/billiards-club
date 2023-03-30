b72a91b8a7a81064af81c94d3f650f570dca0693
Will Garrett <willgarrett610@gmail.com>
2023-03-29 18:55:28 -0600

    Starting ranking page

---
+++ pages/rankings.tsx
@@ -0,0 +1,13 @@
+import { Header } from '@/components/header/header'
+
+export default () => {
+  return (
+    <>
+      <Header />
+      <div className="content">
+        <div className="pageTitle">Player Rankings</div>
+        
+      </div>
+    </>
+  )
+};
