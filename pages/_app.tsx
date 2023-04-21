14b7d4a10f327c473de6b82644e911d6bf6b5e1d
Will Garrett <willgarrett610@gmail.com>
2023-04-21 13:32:59 -0600

    Initialize vercel analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import Head from 'next/head';
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 import { SessionProvider } from 'next-auth/react';
+import { Analytics } from '@vercel/analytics/react';
 
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
@@ -22,17 +23,20 @@ export default function App({
     pageProps: { session, ...pageProps },
 }: AppProps) {
     return (
-        <SessionProvider session={session}>
-            <ThemeProvider theme={darkTheme}>
-                <CssBaseline />
-                <Head>
-                    <title>Billiards Club</title>
-                    <meta name="description" content="Billiards Club Website" />
-                    <meta name="viewport" content="width=device-width, initial-scale=1" />
-                    <link rel="icon" href="/favicon.ico" />
-                </Head>
-                <Component {...pageProps} />
-            </ThemeProvider>
-        </SessionProvider>
+        <>
+            <SessionProvider session={session}>
+                <ThemeProvider theme={darkTheme}>
+                    <CssBaseline />
+                    <Head>
+                        <title>Billiards Club</title>
+                        <meta name="description" content="Billiards Club Website" />
+                        <meta name="viewport" content="width=device-width, initial-scale=1" />
+                        <link rel="icon" href="/favicon.ico" />
+                    </Head>
+                    <Component {...pageProps} />
+                </ThemeProvider>
+            </SessionProvider>
+            <Analytics/>
+        </>
     );
 }
