56503468c9359e5c642f7ee3ca8e922d298cc3e7
Will Garrett <willgarrett610@gmail.com>
2025-02-03 23:07:50 -0700

    Postgres and material react table for rankings

--- pages/rankings.tsx
+++ pages/rankings.tsx
@@ -1,41 +1,46 @@
-import { Header } from '@/components/header/header'
+import { Header } from '@/components/header/header';
 import { Ranking, useGetRankings } from '@/services/playerService';
-import { Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@mui/material';
-
+import { useMemo } from 'react';
+import {
+    MaterialReactTable,
+    useMaterialReactTable,
+    type MRT_ColumnDef, //if using TypeScript (optional, but recommended)
+} from 'material-react-table';
 
 const Rankings = () => {
-  const { data: rankings } = useGetRankings();
+    const { data: rankings } = useGetRankings();
 
-  console.log(rankings);
+    const columns = useMemo(
+        () => [
+            {
+                header: 'Name',
+                accessorKey: 'name',
+            },
+            {
+                header: 'Elo',
+                accessorKey: 'elo',
+            },
+        ],
+        [],
+    );
 
-  return (
-    <>
-      <Header />
-      <div className="content">
-        <div className="pageTitle">Player Rankings</div>
-        <TableContainer component={Paper}>
-          <Table aria-label="rankings table">
-            <TableHead>
-              <TableRow>
-                <TableCell>Rank</TableCell>
-                <TableCell>Player</TableCell>
-                <TableCell>Elo</TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {rankings?.map((ranking: Ranking, index) => (
-                <TableRow key={index}>
-                  <TableCell>{index + 1}</TableCell>
-                  <TableCell>{ranking.name}</TableCell>
-                  <TableCell>{ranking.elo}</TableCell>
-                </TableRow>
-              ))}
-            </TableBody>
-          </Table>
-        </TableContainer>
-      </div>
-    </>
-  )
+    const table = useMaterialReactTable({
+        columns,
+        data: rankings ? rankings : [],
+        enableRowSelection: true,
+        enableColumnOrdering: true,
+        enableGlobalFilter: false,
+    });
+
+    return (
+        <>
+            <Header />
+            <div className="content">
+                <div className="pageTitle">Player Rankings</div>
+                <MaterialReactTable table={table} />
+            </div>
+        </>
+    );
 };
 
 export default Rankings;
